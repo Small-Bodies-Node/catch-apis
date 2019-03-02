987dcd9214983e7bf4ffe9a400a41d10a43c5f4b
dwd-umd <ddarg@umd.edu>
2019-03-02 12:29:52 -0500

    Update jenkinsfile

--- _run_tests.sh
+++ _run_tests.sh
@@ -6,15 +6,3 @@ else
     pytest --junit-xml=pytest_unit.xml --ignore=./tests/test_will_fail.py
 fi
 
-
-
-# if [ $1 = 'jenkins' ]; then
-#     ## Jenkins will error if you use the trick in the else clause
-#     echo "Running from jenkins"
-#     main
-# else
-#     ## Trick to check if this script is being sourced or sh-ed
-#     unset BASH_SOURCE 2>/dev/null
-#     test ".$0" == ".$BASH_SOURCE" && echo "You must <SOURCE> (not SH) this script!!!" || main "$@"
-# fi
-
