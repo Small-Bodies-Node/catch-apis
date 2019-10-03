bf8a50607377a54a3472f6a3440520df5fc5e109
dwd-umd <ddarg@umd.edu>
2019-10-03 16:58:45 -0400

    Add dev deployment env to pipeline; v0

--- src/services/database_provider.py
+++ src/services/database_provider.py
@@ -3,15 +3,16 @@
 """
 
 import typing
-import sqlalchemy
 from contextlib import contextmanager
+import sqlalchemy
 from sqlalchemy.orm.session import Session
 from env import ENV
 
 # Build URI and instantiate data-provider service
 db_engine_URI: str = \
     f"{ENV.DB_DIALECT}://{ENV.DB_USERNAME}:{ENV.DB_PASSWORD}@{ENV.DB_HOST}/{ENV.DB_DATABASE}"
-db_engine: typing.Any = sqlalchemy.create_engine(db_engine_URI)
+db_engine: typing.Any = sqlalchemy.create_engine(
+    db_engine_URI, pool_recycle=3600)
 db_session: typing.Any = sqlalchemy.orm.sessionmaker(bind=db_engine)
 
 
