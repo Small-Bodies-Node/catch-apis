e524ca6c97105722d951b3f9149e70d4a337d82e
Michael S. P. Kelley <msk@astro.umd.edu>
2022-01-27 10:48:09 -0500

    Added matches from name resolution tool.  Fixed reading targets from files.

--- tests/test_target_type_patterns.py
+++ tests/test_target_type_patterns.py
@@ -2,6 +2,7 @@ import os
 import re
 from numpy import full
 import pytest
+import requests
 from services.query import TargetTypePatterns
 
 # Test target names / designations (packed and unpacked) with the
@@ -60,13 +61,34 @@ INTERSTELLAR_OBJECTS = set([
     '2I/Borisov'
 ])
 
+# Add search strings to send to the SBN name resolution tool to this list.
+# Try to get some comets and the interstellar objects.
+RESOLVE_NAMES = [
+    '65P',
+    'Ceres',
+    'encke',
+    'C/2013 US10',
+    'oumuamua',
+    'borisov',
+    '2019 XS'
+]
+
+RESOLVED_NAMES = []
+for query in RESOLVE_NAMES:
+    r = requests.get('https://name-resolution-tool.astro.umd.edu/name',
+                     params={'name': query})
+    matches = r.json()['matches']
+    assert len(matches) > 0
+    RESOLVED_NAMES.extend(matches)
+
+
 # Also add all numbered asteroids from this list
 path = os.path.dirname(__file__)
 with open(f'{path}/asteroid_names.txt', 'r') as inf:
     for line in inf:
         if line[0] == '#':
             continue
-        ASTEROIDS.add(line[:40].strip())
+        ASTEROIDS.add(line[:39].strip())
 
 # Add asteroids, mostly temporary designations, from this list
 path = os.path.dirname(__file__)
@@ -88,12 +110,18 @@ with open(f'{path}/CometEls.txt', 'r') as inf:
         if full_name[0] == 'A':
             ASTEROIDS.add(packed)
             ASTEROIDS.add(full_name)
-        elif full_name[0] == 'I':
+        elif packed[4] == 'I':
             INTERSTELLAR_OBJECTS.add(packed)
             INTERSTELLAR_OBJECTS.add(full_name)
         else:
-            COMETS.add(packed)
             COMETS.add(full_name)
+
+            # permanent designation with a fragment, does not work at Horizons
+            if packed[0].isdigit() and len(packed) == 12:
+                continue
+
+            # we left-strip 0s because Horizons fails for queries like '0001P'
+            COMETS.add(packed.lstrip('0'))
 
 
 @pytest.mark.parametrize('target', ASTEROIDS)
@@ -113,8 +141,30 @@ def test_comet(target):
 
 
 @pytest.mark.parametrize('target', INTERSTELLAR_OBJECTS)
-def test_comet(target):
+def test_interstellar_object(target):
     "Verify that the interstellar object string parses as an ISO and not something else."
     assert re.match(TargetTypePatterns.asteroidal, target) is None
     assert re.match(TargetTypePatterns.cometary, target) is None
     assert re.match(TargetTypePatterns.interstellar_object, target)
+
+
+@pytest.mark.remote
+@pytest.mark.parametrize('match', RESOLVED_NAMES)
+def test_via_name_resolution_tool(match):
+    "Verify that the SBN name resolution tool types match regexp parsing."
+    for match in matches:
+        asteroid = re.match(TargetTypePatterns.asteroidal,
+                            match['target']) is not None
+        comet = re.match(TargetTypePatterns.cometary,
+                         match['target']) is not None
+        iso = re.match(TargetTypePatterns.interstellar_object,
+                       match['target']) is not None
+
+        if match['body_type'] == 'comet':
+            assert comet and not (asteroid or iso)
+        elif match['body_type'] == 'asteroid':
+            assert asteroid and not (comet or iso)
+        elif match['body_type'] == 'interstellar_object':
+            assert iso and not (asteroid or comet)
+        else:
+            raise ValueError('unexpected body_type from name resolution tool.')
