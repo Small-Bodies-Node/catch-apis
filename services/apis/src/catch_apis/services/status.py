b726c0d60da2988a2ee4cb58f85930c1e5d21688
Michael S. P. Kelley <msk@astro.umd.edu>
2022-02-24 15:33:03 -0500

    Only return survey status for queryable sources.

--- services/apis/src/catch_apis/services/status.py
+++ services/apis/src/catch_apis/services/status.py
@@ -3,6 +3,7 @@ from typing import Dict, Union, List
 from catch.model import SurveyStats
 
 from .catch_manager import Catch, catch_manager
+from .. import tasks
 
 
 def sources() -> List[Dict[str, Union[str, int, None]]]:
@@ -13,12 +14,13 @@ def sources() -> List[Dict[str, Union[str, int, None]]]:
     catch: Catch
     with catch_manager() as catch:
         for summary in catch.db.session.query(SurveyStats).all():
-            data.append({
-                'source': summary.source,
-                'source_name': summary.name,
-                'count': summary.count,
-                'start_date': summary.start_date,
-                'stop_date': summary.stop_date,
-                'updated': summary.updated,
-            })
+            if summary.source in tasks.QUERY_SOURCES:
+                data.append({
+                    'source': summary.source,
+                    'source_name': summary.name,
+                    'count': summary.count,
+                    'start_date': summary.start_date,
+                    'stop_date': summary.stop_date,
+                    'updated': summary.updated,
+                })
     return data
--- services/apis/src/catch_apis/tasks/__init__.py
+++ services/apis/src/catch_apis/tasks/__init__.py
@@ -1,1 +1,1 @@
-from .catch_moving_target import catch_moving_target
+from .catch_moving_target import catch_moving_target, QUERY_SOURCES
