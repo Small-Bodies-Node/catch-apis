f2faf32e9ff605ee2018252c8d116a5884ff5e4a
Michael S. P. Kelley <msk@astro.umd.edu>
2024-03-11 11:59:49 -0400

    Restore CORS and move message to services.

--- src/catch_apis/app.py
+++ src/catch_apis/app.py
@@ -1,55 +1,33 @@
 """Entry point to Flask-Connexion API"""
 
 import logging
-from typing import Iterator
 
 import connexion
+from connexion.middleware import MiddlewarePosition
+from starlette.middleware.cors import CORSMiddleware
 from flask import Response
 
-from . import __version__
+from . import __version__ as version
 from .config import allowed_sources, get_logger, ENV
-from .services.queue import RedisConnection
-from .config.env import ENV
-
-
-def messages() -> Iterator[str]:
-    """Iterator for all CATCH-APIs task messages.
-
-    Listens to redis task messaging stream, prints the messages.
-
-    """
-
-    redis: RedisConnection = RedisConnection()
-
-    last: bytes = b'0'
-    wait: int = 3  # seconds between keep alive messages
-    count: int = 0  # number of consecutive keep alive messages
-    while True:
-        message = redis.xread({ENV.REDIS_TASK_MESSAGES: last},
-                              count=1, block=wait * 1000)
-        if len(message) == 0:
-            count += 1
-            if count > (ENV.STREAM_TIMEOUT // wait):
-                yield ': timeout\n\n'
-                return
-            else:
-                yield ': stayin\' alive\n\n'
-                continue
-
-        content: bytes
-        last, content = message[0][1][0]
-        data: str = content.get(b'data', b'').decode()
-        if data != '':
-            count = 0
-            yield f'data: {data}\n\n'
+from .services.stream import messages
 
 
 logger: logging.Logger = get_logger()
 app = connexion.FlaskApp(__name__, specification_dir="api/")
+
+app.add_middleware(
+    CORSMiddleware,
+    position=MiddlewarePosition.BEFORE_EXCEPTION,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.add_api(
     "openapi.yaml",
     arguments={
-        "version": __version__,
+        "version": version,
         "base_href": ENV.BASE_HREF,
         "sources": allowed_sources,
     },
