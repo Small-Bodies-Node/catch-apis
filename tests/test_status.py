58452d9dadfe669c60e1919737b8ec1e8ffbef2e
Michael S. P. Kelley <msk@astro.umd.edu>
2024-08-27 19:28:19 -0400

    Fix test.

--- tests/test_status.py
+++ tests/test_status.py
@@ -4,7 +4,7 @@ from starlette.testclient import TestClient
 from . import fixture_test_client
 
 
-def test_status_sources2(test_client: TestClient):
+def test_status_sources(test_client: TestClient):
     response = test_client.get("/status/sources")
     response.raise_for_status()
     results = response.json()
@@ -15,3 +15,8 @@ def test_status_sources2(test_client: TestClient):
     assert neat_palomar_tricam["count"] == 49 * 4
     assert neat_palomar_tricam["start_date"].startswith("2012-03-14")
     assert neat_palomar_tricam["stop_date"].startswith("2012-03-14")
+
+    (skymapper,) = [row for row in results if row["source"] == "skymapper_dr4"]
+    assert skymapper["count"] == 0
+    assert skymapper["start_date"] is None
+    assert skymapper["stop_date"] is None
