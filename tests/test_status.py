64223c39585d5a28a721162e7f0ea8d7b1f79ea1
Michael S. P. Kelley <msk@astro.umd.edu>
2025-02-14 14:42:36 -0500

    Bugfix: return missing sources, and finish API spec for /status/job_id

--- tests/test_status.py
+++ tests/test_status.py
@@ -1,10 +1,13 @@
 # Licensed with the 3-clause BSD license.  See LICENSE for details.
 
+import uuid
 from starlette.testclient import TestClient
-from . import fixture_test_client
+from astropy.time import Time
+from catch_apis.tasks.catch import catch_task
+from . import fixture_test_client, mock_redis, mock_flask_request
 
 
-def test_status_sources2(test_client: TestClient):
+def test_status_sources(test_client: TestClient):
     response = test_client.get("/status/sources")
     response.raise_for_status()
     results = response.json()
@@ -16,3 +19,43 @@ def test_status_sources2(test_client: TestClient):
     assert neat_palomar_tricam["start_date"].startswith("2012-03-14")
     assert neat_palomar_tricam["stop_date"].startswith("2012-03-14")
     assert neat_palomar_tricam["nights"] == 1
+
+
+def test_status_job_id(
+    test_client: TestClient, mock_redis, mock_flask_request, monkeypatch
+):
+    job_id = uuid.uuid4()
+    catch_task(job_id, "65P", ["neat_palomar_tricam"], None, None, False, 0, True)
+
+    response = test_client.get(f"/status/{job_id.hex}")
+    response.raise_for_status()
+    results = response.json()
+    assert results["job_id"] == job_id.hex
+    expected = {
+        "padding": 0,
+        "start_date": None,
+        "stop_date": None,
+        "target": "65P",
+        "sources": ["neat_palomar_tricam"],
+        "uncertainty_ellipse": False,
+    }
+    for k, v in expected.items():
+        assert results["parameters"][k] == v
+
+    expected = {
+        "count": 0,
+        "source": "neat_palomar_tricam",
+        "source_name": "NEAT Palomar Tricam",
+        "status": "finished",
+    }
+    for k, v in expected.items():
+        assert results["status"][0][k] == v
+
+    # date should be parsable by Time
+    Time(results["status"][0]["date"])
+
+    assert results["status"][0]["execution_time"] > 0
+
+    # test invalid job id
+    response = test_client.get(f"/status/invalid_job_id")
+    assert response.status_code == 400
