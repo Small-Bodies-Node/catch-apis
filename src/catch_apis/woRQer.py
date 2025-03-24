19d4907f8c2dc4a2ace4444ecc80b9f7266c768e
Michael S. P. Kelley <msk@astro.umd.edu>
2025-03-24 13:47:43 -0400

    Dependency updates.

--- src/catch_apis/woRQer.py
+++ src/catch_apis/woRQer.py
@@ -1,18 +1,17 @@
 """
-    Script to launch a new rq worker listening to JOBS queue.
+Script to launch a new rq worker listening to JOBS queue.
 """
 
-from rq import Worker as WoRQer, Queue, Connection
+from rq import Worker as WoRQer
 
-from .services.queue import RedisConnection, JobsQueue
+from .services.queue import JobsQueue
 
 
 def run(burst: bool = False):
     """Set burst to True and the worker will quit when the queue is empty."""
-    with Connection(RedisConnection()):
-        queue: Queue = JobsQueue()
-        woRQer: WoRQer = WoRQer(queue, default_worker_ttl=1000)
-        woRQer.work(burst=burst)
+    queue = JobsQueue()
+    woRQer = WoRQer(queue, worker_ttl=1000)
+    woRQer.work(burst=burst)
 
 
 if __name__ == "__main__":
